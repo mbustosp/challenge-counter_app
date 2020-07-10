a570a669317eaa0b50c8b656a91ee7026986276b
mbustosp <agustinbustos_com@hotmail.com>
2020-07-10 03:04:37 -0400

    Adds story for base styles

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../src/**/*.stories.js'],
+  stories: ['../src/**/*.stories.js[x]'],
   addons: [
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
