971571c86bc1077e1520e5040595a3098768c7c7
mbustosp <agustinbustos_com@hotmail.com>
2020-07-09 23:45:48 -0400

    Adds prettier and eslinter
    Additionally, airbnb style rules are included

--- src/index.jsx
+++ src/index.jsx
@@ -2,21 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // You don't have to use `fetch` btw, use whatever you want
-const getCounters = () => 
-  fetch('/api/v1/counter', { method: 'get' })
-    .then(res => res.json());
+const getCounters = () => fetch('/api/v1/counter', { method: 'get' }).then((res) => res.json());
 
 const App = () => {
   React.useEffect(() => {
     getCounters().then(console.log, console.error);
   }, []);
 
-  return (
-    <h1>Hello, Cornershop!</h1>
-  );
+  return <h1>Hello, Cornershop!</h1>;
 };
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
