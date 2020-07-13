94456c0cb54c38e73ee35564baa41d27a8c536ea
mbustosp <agustinbustos_com@hotmail.com>
2020-07-13 00:07:17 -0400

    Adds knobs addon and update stories

--- src/components/atoms/atoms.stories.jsx
+++ src/components/atoms/atoms.stories.jsx
@@ -1,13 +1,16 @@
 /**
  * Base dependencies
  */
-import React, { useState } from 'react';
+import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { text, boolean } from '@storybook/addon-knobs';
+import { withKnobs } from '@storybook/addon-knobs/react';
 import ActionButton from './ActionButton';
 import Button from './Button';
 import TextInput from './TextInput';
 import SearchInput from './SearchInput';
+import CancelButton from './CancelButton';
 
 /**
  * Styles
@@ -15,38 +18,51 @@ import SearchInput from './SearchInput';
 import '../../common/_base.scss';
 
 storiesOf('Atoms', module)
+  .addDecorator(withKnobs)
   .add('Button - Non styled', () => {
-    return <Button onClick={action('Clicked')} label="I am button" />;
+    return (
+      <Button
+        onClick={action('Clicked')}
+        disabled={boolean('disabled', false)}
+        label={text('label', 'I am a button')}
+      />
+    );
   })
   .add('Button - Action', () => {
-    return <ActionButton label="I am an action button" onClick={action('Clicked!')} />;
-  })
-  .add('Button - Action - Light', () => {
     return (
-      <ActionButton lightTheme label="I am a light action button" onClick={action('Clicked!')} />
+      <ActionButton
+        label={text('label', 'I am an action button')}
+        lightTheme={boolean('lightTheme', false)}
+        onClick={action('Clicked!')}
+        disabled={boolean('disabled', false)}
+      />
     );
   })
-  .add('Text Input - Empty', () => {
-    return <TextInput />;
-  })
-  .add('Text Input - With placeholder', () => {
-    return <TextInput placeholder="Cups of coffee" />;
-  })
-  .add('Text Input - With placeholder - Dynamic', () => {
-    const [text, setText] = useState('');
+  .add('Button - Cancel', () => {
     return (
-      <TextInput placeholder="Cups of coffee" value={text} onChange={(value) => setText(value)} />
+      <CancelButton
+        label={text('label', 'I am a cancel button')}
+        onClick={action('Clicked!')}
+        disabled={boolean('disabled', false)}
+      />
     );
   })
-  .add('Search Input - Empty', () => {
-    return <SearchInput />;
-  })
-  .add('Search Input - With placeholder', () => {
-    return <SearchInput placeholder="Cups of coffee" />;
-  })
-  .add('Search Input - With placeholder - Dynamic', () => {
-    const [text, setText] = useState('');
+  .add('Text Input', () => {
     return (
-      <SearchInput placeholder="Cups of coffee" value={text} onChange={(value) => setText(value)} />
+      <TextInput
+        placeholder={text('placeholder', 'Cups of coffee')}
+        value={text('value', '')}
+        removeDefaultStyles={boolean('removeDefaultStyles', false)}
+        disabled={boolean('disabled', false)}
+      />
+    );
+  })
+  .add('Search Input', () => {
+    return (
+      <SearchInput
+        placeholder={text('text', 'Cups of coffee')}
+        value={text('value', '')}
+        disabled={boolean('disabled', false)}
+      />
     );
   });
