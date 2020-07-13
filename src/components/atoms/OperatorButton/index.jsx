bfb063dc2b73959e13d0a75af3431660d2a8dc66
mbustosp <agustinbustos_com@hotmail.com>
2020-07-13 12:31:37 -0400

    Updates prettier config and Adds Operator Button
    Adds disabled state and changes svg for font icons

---
+++ src/components/atoms/OperatorButton/index.jsx
@@ -0,0 +1,49 @@
+/**
+ * Core dependencies
+ */
+import React from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import Button from '../Button';
+
+/**
+ * Styles
+ */
+import './OperatorButton.scss';
+
+export const OPERATION_TYPE = {
+  PLUS: 'PLUS',
+  MINUS: 'MINUS',
+};
+
+const OperatorButton = ({ onClick, className, operation, disabled }) => {
+  return (
+    <Button
+      className={classNames(
+        'button-operator',
+        { 'button-operator--disabled': disabled },
+        className,
+      )}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {operation === OPERATION_TYPE.PLUS ? (
+        <span className="icon icon-plus" />
+      ) : (
+        <span className="icon icon-minus" />
+      )}
+    </Button>
+  );
+};
+
+OperatorButton.propTypes = {
+  ...Button.propTypes,
+  operation: PropTypes.oneOf(Object.values(OPERATION_TYPE)),
+};
+
+OperatorButton.defaultProps = {
+  ...Button.defaultProps,
+  operation: OPERATION_TYPE.PLUS,
+};
+
+export default OperatorButton;
